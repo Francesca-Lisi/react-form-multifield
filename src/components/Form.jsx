881feaa9bcc5dde4aaaf5b41977325a46e92de09
Francesca-Lisi <francescalisi2143@gmail.com>
2025-01-22 19:58:24 +0100

    feat: add tags and categories with map

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,81 +1,123 @@
+// creo gli array che mi servono per mappare il form
+const categories = [
+  'Frontend',
+  'Backend',
+  'Fullstack',
+  'DevOps',
+  'Mobile'
+]
+const tags = [
+  { id: 1, name: 'HTML' },
+  { id: 2, name: 'CSS' },
+  { id: 3, name: 'JavaScript' },
+  { id: 4, name: 'React' }
+]
+
+//importo useState
+import { useState } from 'react';
+
 
 
 const Form = () => {
+  // Definisco l'oggetto vuoto che andrò a compilare con il form
+  const initialformData = {
+    title: '',
+    urlImage: '',
+    content: '',
+    category: '',
+    tags: [], // array di stringhe
+    published: false
+  }
+
+  //inserisco le costanti di stato:
+  const [articleList, setArticleList] = useState(initialformData);
+  const [newListItem, setNewListItem] = useState('');
+
+  const handlerSubmit = (e) => {
+    e.preventDefault();
+    //creo un clone dell'array e aggiungo il nuovo elemento
+    setArticleList([newListItem, ...articleList])
+  }
+
+
+
   return (
     <div className="card p-4">
+      <h3 className='mb-4'>Inserisci un nuovo articolo</h3>
       <form action="#">
-        <div class="row mb-3">
-          <label for="title" className="col-sm-2 col-form-label">Titolo</label>
-          <div class="col-sm-10">
+        <div className="row mb-3">
+          <label for="title" className="col-sm-2 col-form-label fw-semibold">Titolo</label>
+          <div className="col-sm-10">
             <input type="text" className="form-control" id="title" />
           </div>
         </div>
-        <div class="row mb-3">
-          <label for="url" className="col-sm-2 col-form-label">URL immagine</label>
-          <div class="col-sm-10">
+        <div className="row mb-3">
+          <label for="url" className="col-sm-2 col-form-label fw-semibold">URL immagine</label>
+          <div className="col-sm-10">
             <input type="text" className="form-control" id="url" />
           </div>
         </div>
-        <div class="row mb-3">
-          <label for="content" className="col-sm-2 col-form-label">Contenuto</label>
-          <div class="col-sm-10">
+        <div className="row mb-3">
+          <label for="content" className="col-sm-2 col-form-label fw-semibold">Contenuto</label>
+          <div className="col-sm-10">
             <textarea type="text" className="form-control" id="content" ></textarea>
           </div>
         </div>
-        <div class="row mb-4">
-          <label for="content" className="col-sm-2 col-form-label">Categoria</label>
-          <div class="col-sm-10">
-            <select type="form-select" className="form-control" id="content" >
+        <div className="row mb-4">
+          <label for="content" className="col-sm-2 col-form-label fw-semibold">Categoria</label>
+          <div className="col-sm-10">
+            <select
+              className="form-select"
+
+              name="category"
+              defaultValue=""
+            >
               <option selected>Seleziona una categoria</option>
-              <option value="1">Fullstack</option>
-              <option value="2">Frontend</option>
-              <option value="3">Backend</option>
-              <option value="4">Mobile</option>
+              {categories.map((category, index) => (
+                <option key={index} value={index}>{category}</option>
+              ))}
             </select>
           </div>
         </div>
-        <fieldset class="row mb-3">
-          <legend class="col-form-label col-sm-2 pt-0">Tags</legend>
-          <div class="col-sm-10">
-            <div class="form-check">
-              <input class="form-check-input" type="checkbox" name="gridRadios" id="gridRadios1" value="option1" />
-              <label class="form-check-label" for="gridRadios1">
-                HTML
-              </label>
-            </div>
-            <div class="form-check">
-              <input class="form-check-input" type="checkbox" name="gridRadios" id="gridRadios2" value="option2" />
-              <label class="form-check-label" for="gridRadios2">
-                CSS
-              </label>
-            </div>
-            <div class="form-check">
-              <input class="form-check-input" type="checkbox" name="gridRadios" id="gridRadios2" value="option2" />
-              <label class="form-check-label" for="gridRadios2">
-                React
-              </label>
-            </div>
-            <div class="form-check disabled">
-              <input class="form-check-input success" type="checkbox" name="gridRadios" id="gridRadios3" value="option3" />
-              <label class="form-check-label" for="gridRadios3">
-                JavaScript
-              </label>
-            </div>
+        <fieldset className="row mb-3">
+          <legend className="col-form-label col-sm-2 pt-0 fw-semibold">Tags</legend>
+          <div className="col-sm-10">
+            {tags.map(tag => (
+              <div className="form-check">
+                <input
+                  className="form-check-input"
+                  type="checkbox"
+                  id={`tag${tag.id}`}
+                  value={tag.name}
+                />
+                <label className="form-check-label" htmlFor={`tag${tag.id}`}>
+                  {tag.name}
+                </label>
+              </div>
+            ))}
+
+
           </div>
         </fieldset>
-        <fieldset class="row mb-3">
-          <legend class="col-form-label col-sm-2 pt-0">Stato</legend>
-          <div class="col-sm-10">
-            <div class="form-check">
-              <input class="form-check-input" type="checkbox" name="gridRadios" id="gridRadios1" value="option1" />
-              <label class="form-check-label" for="gridRadios1">
+        <fieldset className="row mb-3">
+          <legend className="col-form-label col-sm-2 pt-0 fw-semibold">Stato</legend>
+          <div className="col-sm-10">
+            <div className="form-check">
+              <input
+                className="form-check-input"
+                type="checkbox"
+                name="published"
+                id="published"
+              // checked={}
+              />
+              <label className="form-check-label" for="gridRadios1">
                 Pubblicato
               </label>
             </div>
           </div>
         </fieldset>
         <div className="d-flex justify-content-center">
-          <button type="submit" className="btn btn-outline-primary px-4">Invio</button>
+          <button type="submit" onSubmit={handlerSubmit} className="btn btn-outline-primary px-4">Invio</button>
         </div>
       </form>
     </div>
